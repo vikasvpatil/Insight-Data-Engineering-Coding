8a8dbe6f476efa4d753002a5d59207fb9653dc91
Vikas V Patil <vikasvpatil@outlook.com>
2015-03-25 11:41:40 -0600

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+chmod a+x C:/Users/Vikas/Desktop/DEC/src/wordcount_median.py
+
+python C:/Users/Vikas/Desktop/DEC/src/wordcount_median.py C:/Users/Vikas/Desktop/DEC/wc_input C:/Users/Vikas/Desktop/DEC/wc_output/wc_result.txt
+python C:/Users/Vikas/Desktop/DEC/src/wordcount_median.py C:/Users/Vikas/Desktop/DEC/wc_input C:/Users/Vikas/Desktop/DEC/wc_output/med_result.txt
